b8a19355d8c6fd80aa2a98e6a3f0e7dec102e46e
Elik Katz <elikkatz@gmail.com>
2019-08-13 17:51:19 +0300

    Update script

--- projectName2projectId.sh
+++ projectName2projectId.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #WhiteSource - Elik Katz
 
-if [ $# -ne 1 ]; then
-        echo " Run like this: ./projectName2projrctId.sh {apikey} "
+if [ $# -ne 2 ]; then
+        echo " Run like this: ./projectName2projrctId.sh {site:saas/app/app-eu} {apikey} "
         exit 1
 else
-		apikey=$1
+		site=$1
+		apikey=$2
         echo " Punch in the project name to search " ; read p_name
-        curl -s -H "Content-Type: application/json" https://saas.whitesourcesoftware.com/api -d '{"requestType" : "getOrganizationProjectVitals","orgToken" : '"$apikey"'}' |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' |grep -B1 "$p_name" |tac
+        curl -s -H "Content-Type: application/json" https://$site.whitesourcesoftware.com/api -d '{"requestType" : "getOrganizationProjectVitals","orgToken" : '"$apikey"'}' |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' |grep -B1 "$p_name" |tac
 fi
